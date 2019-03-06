27967b7c43dc73c1d9d05d90a5be327a6a4058e4
alexgpitts <46640378+alexgpitts@users.noreply.github.com>
2019-03-05 19:48:09 -0800

    added all character pages
    major update

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Apex Characters' });
 });
 
-module.exports = router;
+router.get('/Bangalore', function(req, res, next) {
+  res.render('bangalore', { title: "Bangalore" });
+});
 
+router.get('/Bloodhound', function(req, res, next) {
+  res.render('Bloodhound', { title: "Bloodhound" });
+});
+
+router.get('/Caustic', function(req, res, next) {
+  res.render('Caustic', { title: "Caustic" });
+});
+
+router.get('/Gibraltar', function(req, res, next) {
+  res.render('Gibraltar', { title: "Gibraltar" });
+});
+
+router.get('/Lifeline', function(req, res, next) {
+  res.render('Lifeline', { title: "Lifeline" });
+});
+
+router.get('/Mirage', function(req, res, next) {
+  res.render('Mirage', { title: "Mirage" });
+});
+
+router.get('/Pathfinder', function(req, res, next) {
+  res.render('Pathfinder', { title: "Pathfinder" });
+});
+
+router.get('/Wraith', function(req, res, next) {
+  res.render('Wraith', { title: "Wraith" });
+});
+
+module.exports = router;
 
